7b9d65b7f538f8b30ca0ab05e8a09823df57036d
Charl <charlsmitemail@gmail.com>
2023-09-12 13:10:38 +0200

    Replace all

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,7 +45,8 @@ const appRoutes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     HousingService
--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.scss']
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit {
+
+  registrationForm: FormGroup = new FormGroup({})
+  constructor() {}
+
+  ngOnInit() {
+    this.registrationForm = new FormGroup( {
+      userName: new FormControl('Mark', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm)
+  }
+
 
 }
