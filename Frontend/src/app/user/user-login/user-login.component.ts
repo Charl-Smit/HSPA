9523fc935e54859888ef9bb8c81b290415503901
Charl <charlsmitemail@gmail.com>
2023-09-21 13:43:10 +0200

    Replace all

--- Frontend/src/app/user/user-login/user-login.component.ts
+++ Frontend/src/app/user/user-login/user-login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit {
+  constructor() {}
 
+  userSubmitted = false;
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  onLogin() {
+    this.userSubmitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
